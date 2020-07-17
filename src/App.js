94a86e94c8f5321cc9d6dc23a22896cf18c902ae
abscess <loser-pk@yandex.com>
2020-07-17 13:42:32 +0300

    [Initial]

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
 import React from 'react';
-import logo from './logo.svg';
+import Markdown from 'markdown-to-jsx';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Link from '@material-ui/core/Link';
+import Paper from '@material-ui/core/Paper';
 import './App.css';
 
+const Image = (props) => {
+  const { children, alt, ...otherProps } = props;
+  return (<span className={"image"}><img alt={alt} {...otherProps}>{children}</img></span>)
+};
+
+
 function App() {
+  const [content, setContent] = React.useState("");
+  fetch("https://raw.githubusercontent.com/ReactNativeNews/React-Native-Apps/master/README.md").then((res) => res.text()).then((data) => setContent(data));
+  const linkOriginal = "https://github.com/ReactNativeNews/React-Native-Apps";
+  const companyLink = "https://infinite.red/technologies/react-native";
+  const icons8Link = "https://icons8.com";
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={"body"}>
+      <Paper elevation={3} className={"list"}>
+        <div className={"original"}>
+          <Link href={linkOriginal}>Original </Link>
+          by <Link href={companyLink}>Infinite Red</Link>
+        </div>
+        <TableContainer>
+          <Markdown options={{
+            overrides: {
+              img: Image,
+              table: Table,
+              tbody: TableBody,
+              thead: TableHead,
+              tr: TableRow,
+              td: TableCell,
+              a: Link
+            }
+          }}>{content}</Markdown>
+        </TableContainer>
+        <div className={"icon-link"}>
+          Icons by <Link href={icons8Link}> Icons8</Link>
+        </div>
+      </Paper>
     </div>
   );
 }
